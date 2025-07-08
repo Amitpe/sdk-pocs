eceb09d1fe908acc4c4be8921f3928e11064867d
amitp <amitp@wix.com>
2025-07-08 16:13:08 +0300

    full poc

--- src/query-overload.ts
+++ src/query-overload.ts
@@ -1,10 +1,12 @@
 import { productsV3 } from "@wix/stores";
 import { cart } from "@wix/ecom";
-import { createClient, OAuthStrategy } from "@wix/sdk";
+import { OAuthStrategy, createClient } from "@wix/sdk";
+import { members } from "@wix/members";
+
 
 const myWixClient = createClient({
-    modules: { productsV3 },
-    headers: { 'Authorization': '1HYkPEXwjn4uS9c9fkGqrhm26X3iU_iU3piFoFQNTQo.eyJpbnN0YW5jZUlkIjoiM2FmZTdmZmItYmRhYi00YWJhLTkyMTctNzRjMzhjYTFhMTQyIiwiYXBwRGVmSWQiOiIxMzgwYjcwMy1jZTgxLWZmMDUtZjExNS0zOTU3MWQ5NGRmY2QiLCJtZXRhU2l0ZUlkIjoiODY4NmViMmYtZWEyNC00ZTkzLWIwODEtZDQxMjhmZTk1ZTE0Iiwic2lnbkRhdGUiOiIyMDI1LTA3LTA3VDEwOjUwOjExLjgzNFoiLCJ1aWQiOiI4Zjk3ZDE3ZS1kNmEzLTQ0ZDEtYTE0OC05M2RkNTAwZjYzOTciLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwidmVuZG9yUHJvZHVjdElkIjoic3RvcmVzX3NpbHZlciIsImRlbW9Nb2RlIjpmYWxzZSwib3JpZ2luSW5zdGFuY2VJZCI6Ijc5ZmU5YTY3LTk5NzEtNDVkNy1iNTI0LTFhMWRhMWQ1NzA3OCIsImJpVG9rZW4iOiJiYzc4OTRkNC01NzhmLTA0MjktMjI5Ni1hMGQxMDM0OGZmNTYiLCJzaXRlT3duZXJJZCI6IjhmOTdkMTdlLWQ2YTMtNDRkMS1hMTQ4LTkzZGQ1MDBmNjM5NyIsInNpdGVNZW1iZXJJZCI6IjhmOTdkMTdlLWQ2YTMtNDRkMS1hMTQ4LTkzZGQ1MDBmNjM5NyIsImV4cGlyYXRpb25EYXRlIjoiMjAyNS0wNy0wN1QxNDo1MDoxMS44MzRaIiwibG9naW5BY2NvdW50SWQiOiI4Zjk3ZDE3ZS1kNmEzLTQ0ZDEtYTE0OC05M2RkNTAwZjYzOTciLCJwYWkiOm51bGwsImxwYWkiOm51bGwsImFvciI6dHJ1ZSwic2NkIjoiMjAyNS0wNC0xMFQwNjo0NToyNi4xOTdaIiwiYWNkIjoiMjAxNi0wOC0wMlQxMjozNjoxNFoifQ' }
+    modules: { productsV3, members },
+    headers: { 'Authorization': '3lEmehalMcKEzcBk8la2NHE0f6GXyJWdq-CQ9iY3A00.eyJpbnN0YW5jZUlkIjoiODY4NmViMmYtZWEyNC00ZTkzLWIwODEtZDQxMjhmZTk1ZTE0IiwiYXBwRGVmSWQiOiIyMmJlZjM0NS0zYzViLTRjMTgtYjc4Mi03NGQ0MDg1MTEyZmYiLCJtZXRhU2l0ZUlkIjoiODY4NmViMmYtZWEyNC00ZTkzLWIwODEtZDQxMjhmZTk1ZTE0Iiwic2lnbkRhdGUiOiIyMDI1LTA3LTA4VDExOjIyOjQ1LjAxNloiLCJ1aWQiOiI4Zjk3ZDE3ZS1kNmEzLTQ0ZDEtYTE0OC05M2RkNTAwZjYzOTciLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiZGVtb01vZGUiOmZhbHNlLCJzaXRlT3duZXJJZCI6IjhmOTdkMTdlLWQ2YTMtNDRkMS1hMTQ4LTkzZGQ1MDBmNjM5NyIsInNpdGVNZW1iZXJJZCI6IjhmOTdkMTdlLWQ2YTMtNDRkMS1hMTQ4LTkzZGQ1MDBmNjM5NyIsImV4cGlyYXRpb25EYXRlIjoiMjAyNS0wNy0wOFQxNToyMjo0NS4wMTZaIiwibG9naW5BY2NvdW50SWQiOiI4Zjk3ZDE3ZS1kNmEzLTQ0ZDEtYTE0OC05M2RkNTAwZjYzOTciLCJhb3IiOnRydWUsInNjZCI6IjIwMjUtMDQtMTBUMDY6NDU6MjYuMTk3WiIsImFjZCI6IjIwMTYtMDgtMDJUMTI6MzY6MTRaIn0' }
     // auth: OAuthStrategy({
     //     clientId: "22bef345-3c5b-4c18-b782-74d4085112ff",
     // })
@@ -12,17 +14,23 @@ const myWixClient = createClient({
 
 
 async function queryProducts() {
+    // arguments = 0, always builder
     const res1 = (await myWixClient.productsV3.queryProducts().ascending('_createdDate').find()).items
     console.log('**** RES 1 ****************')
     console.log(res1)
     console.log('************************************************')
 
+    
+    // arguments = 1, runtime check on the argument if it fits CursorQuery or not
+    // in this case it's not CursorQuery, so it's a builder
     const res2 = (await myWixClient.productsV3.queryProducts({ fields: ["ALL_CATEGORIES_INFO"] }).ascending('_createdDate').find()).items
     console.log('**** RES 2 ****************')
     console.log(res2)
     console.log('************************************************')
+    
 
-
+    // arguments = 1, runtime check on the argument if it fits CursorQuery or not
+    // In this case it's CursorQuery, so it's the new query overload
     console.log('**** RES 3 ****************')
     const res3 = (await myWixClient.productsV3.queryProducts({
         sort:
@@ -36,8 +44,18 @@ async function queryProducts() {
     console.log(res3)
     console.log('************************************************')
 
+    
+    // arguments = 1, runtime check on the argument if it fits CursorQuery or not
+    // In this case it's an empty object, so it's the new query overload
+    console.log('**** RES 3 ****************')
+    const res4 = (await myWixClient.productsV3.queryProducts({})).products;
+    console.log(res4)
+    console.log('************************************************')
+
+
+    // arguments = 2, always new query overload
     console.log('**** RES 4 ****************')
-    const res4 = (await myWixClient.productsV3.queryProducts({ sort:
+    const res5 = (await myWixClient.productsV3.queryProducts({ sort:
         [
             {
                 fieldName: 'createdDate',
@@ -45,11 +63,11 @@ async function queryProducts() {
             },
         ]
      }, { fields: ["ALL_CATEGORIES_INFO"] })).products;
-    console.log(res4)
+    console.log(res5)
     console.log('************************************************')
 
     console.log(JSON.stringify(res1) === JSON.stringify(res3))
-    console.log(JSON.stringify(res2) === JSON.stringify(res4))
+    console.log(JSON.stringify(res2) === JSON.stringify(res5))
 }
 
 queryProducts()
